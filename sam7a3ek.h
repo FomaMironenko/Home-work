b60e3bd826510ca3f335138e6603efc848f912e9
FomaMironenko <fomius2000@yandex.ru>
2016-10-10 21:24:40 +0300

    Add files via upload

---
+++ sam7a3ek.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout<<"введите два числа";
+  cin>>a>>b;
+  int c=a*b;
+  while(a!=0; b!=0)
+  {
+      if a>b a=a%b
+      else b=b%a;
+  }
+  if a>b a=0
+  else b=0;
+  int d=c/(a+b);
+  cout<<d;
+}
